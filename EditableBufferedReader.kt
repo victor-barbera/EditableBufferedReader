e72ef634cdfbd2a7b6f5833f98edb11aef477f0f
Carmen Blanco Perez <MACA@10-192-44-230client.eduroam.upc.edu>
2020-09-30 17:14:03 +0200

    Creació projecte i estructura

---
+++ EditableBufferedReader.kt
@@ -0,0 +1,17 @@
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class EditableBufferedReader(input: InputStreamReader) : BufferedReader(input){
+    fun setRaw(){
+
+    }
+    fun unsetRaw(){
+
+    }
+    override fun read() : int {
+
+    }
+    override fun readLine() : String {
+
+    }
+}
